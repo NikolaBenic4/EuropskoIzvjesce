0a10e9c34be8a97151a258076380e0eea4ddf628
NikolaBenic4 <nikolabenic13@gmail.com>
2025-08-15 11:17:18 +0200

    Promjena

--- europskoIzvjesceWeb/src/App.jsx
+++ europskoIzvjesceWeb/src/App.jsx
@@ -1,24 +1,69 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import './css/App.css';
-// import FullForm from './pages/FullForm';   // privremeno zakomentirano
+import { FaQrcode, FaBolt, FaMapMarkerAlt, FaLock, FaRegThumbsUp, FaCarCrash } from "react-icons/fa";
+// import FullForm from './pages/FullForm';
 
 function Home() {
   return (
     <>
       <header className="site-header"><h1>Europsko Izvješće</h1></header>
-      <div className="hero-containter">
+      <div className="hero-container">
         <img
-        src="/public/assets/NesrecaSlika.png"
-        className="hero-image"
-        ></img>
+          src="/assets/NesrecaSlika.png"
+          className="hero-image"
+          alt="Prometna nesreća"
+        />
       </div>
       <section className="content-section">
         <h1 className="content-title">Digitalno Europsko izvješće</h1>
         <p className="content-subtitle">
           Prijavite i dokumentirajte prometnu nesreću na jednostavan i siguran način.
         </p>
-        {/* dok ne popravite forme, vodimo samo na Home */}
         <Link to="/" className="cta-button">Započni prijavu</Link>
+      </section>
+      <br/>
+      <section className="how-it-works">
+        <h2>Kako funkcionira?</h2>
+        <p className="how-desc">
+          Jednostavno i sigurno dokumentirajte prometnu nesreću online.<br />
+          Cijeli proces obavljate u nekoliko brzih koraka bez potrebe za papirologijom.
+        </p>
+        <ol className="how-steps">
+          <li>Unesite osnovne podatke o nesreći.</li>
+          <li>Dodajte informacije o sudionicima i vozilima.</li>
+          <li>Ako postoje, unesite podatke o svjedocima</li>
+          <li>Opišite okolnosti nesreće.</li>
+          <li>Pregledajte i potvrdite izvješće digitalnim potpisom</li>
+          <li>Pošaljite izvješće izravno osiguravajućem društvu.</li>
+        </ol>
+      </section>
+      <section className="benefits-section">
+        <div className="benefits-container">
+          <div className="benefit-item">
+            <FaQrcode className="benefit-icon" />
+            <span>Pristup stranici kroz QR kod</span>
+          </div>
+          <div className="benefit-item">
+            <FaBolt className="benefit-icon" />
+            <span>Automatsko ispunjavanje polja</span>
+          </div>
+          <div className="benefit-item">
+            <FaMapMarkerAlt className="benefit-icon" />
+            <span>Precizno pronalaženje lokacije nesreće</span>
+          </div>
+          <div className="benefit-item">
+            <FaLock className="benefit-icon" />
+            <span>Sigurnost podataka</span>
+          </div>
+          <div className="benefit-item">
+            <FaRegThumbsUp className="benefit-icon" />
+            <span>Jednostavnost unosa</span>
+          </div>
+          <div className="benefit-item">
+            <FaCarCrash className="benefit-icon" />
+            <span>3D sken automobilske nesreće</span>
+          </div>
+        </div>
       </section>
     </>
   );
